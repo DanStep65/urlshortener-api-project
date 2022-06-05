5383081cff746748d67ce66240550e99fae2a60b
Daniel Stephen <73526667+DanStep65@users.noreply.github.com>
2022-06-05 16:48:19 +0000

    Changed replit config so nodemon won't break the file. Finished url verification and short-url record with a new json file.

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+//add a new custom file that records all of the shorturls, must contain 1 json to prevent error
+const shorturl = require('./shorturl');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
-
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
@@ -18,6 +23,36 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.route('/api/shorturl')
+  .post((req, res) => {
+    //create an empty array that will contain shorturl records and the new shorturl from the post
+    // const array = [];
+    // array.push(shorturl);
+    
+    //regex taken from cloudhadoop and stackoverflow, used to verify the url
+    const regexpression = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+
+    if(regexpression.test(req.body.url)){
+      let json_req = {
+        original_url: req.body.url,
+        short_url: shorturl.length + 1
+      };
+      shorturl.push(json_req);
+      //write the shorturl record in shorturl.json
+      fs.writeFile('shorturl.json', JSON.stringify(shorturl), (err) => {
+        if (!err) {
+          console.log('done');
+        }
+      });
+      res.json(json_req);      
+    }
+    else{
+      res.json({"error":"invalid url"});
+    }
+  });
+
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
