8dab25cb05bb5996c1047c15575d25acf9152f47
Daniel Stephen <73526667+DanStep65@users.noreply.github.com>
2022-06-06 15:28:08 +0000

    Made the api can redirect browser to original_url when id of short_url exists in the shorturl.json, while adding some documents.

--- index.js
+++ index.js
@@ -29,31 +29,49 @@ app.post('/api/shorturl/', (req, res) => {
     //regex taken from cloudhadoop and stackoverflow, used to verify the url
     const regexpression = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
     if(regexpression.test(req.body.url)){
+      //when body.url passed the test, filter out the shortened url record and see if there are matches
       let url_done = shorturl.filter(obj => {
         return obj.original_url == req.body.url
       });
+      //if none matches
       if(url_done.length === 0){
+        //add the body url to an object with its short_url id
         let json_req = {
           original_url: req.body.url,
           short_url: shorturl.length + 1
         };
+        //push the body url to the existing json in the shorturl
         shorturl.push(json_req);
-        //write the shorturl record in shorturl.json
+        //write the pushed shorturl record back in shorturl.json
         fs.writeFile('shorturl.json', JSON.stringify(shorturl), (err) => {
           if(err) return console.log(err);
         });
+        //send the object to the browser
         res.json(json_req);      
       }
       else{
+        //return the one and only object from the filtered array of original_url that matched the body.url to the browser
         res.json(url_done[0]);
       }
     }
     else{
+      //return error object
       res.json({"error":"invalid url"});
     }
   });
 app.get('/api/shorturl/:num', (req, res) => {
-  
+  //filter out the shortened url record based on the short_url id and see if there are matches
+  const url = shorturl.filter(obj => {
+    return req.params.num == obj['short_url'];
+  });
+  //if none found, return "Not Found"
+  if(url.length === 0){
+    res.send('Not Found');
+  }
+  else{
+    //redirect the browser to the original url.
+    res.redirect(url[0]['original_url']);
+  }
 });
 
 
